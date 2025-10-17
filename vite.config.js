0d08bcedb3a7679ae23f44b20e761f76240cfb1d
WeiLin <nomorecomputer@gmail.com>
2025-10-17 11:40:32 +0800

    init2

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: "/js_w5/",
+  base: "/js_w6/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
