a2044777631075d1d77d8789dca5f03afeed360a
WeiLin <nomorecomputer@gmail.com>
2025-10-20 15:18:20 +0800

    Updates

---
+++ assets/index-Crvx0arL.js
@@ -0,0 +1,35 @@
+(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function r(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(t){if(t.ep)return;t.ep=!0;const i=r(t);fetch(t.href,i)}})();const s=[{id:0,name:"肥宅心碎賞櫻3日",imgUrl:"https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",area:"高雄",description:"賞櫻花最佳去處。肥宅不得不去的超讚景點！",group:87,price:1400,rate:10},{id:1,name:"貓空纜車雙程票",imgUrl:"https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",area:"台北",description:"乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",group:99,price:240,rate:2},{id:2,name:"台中谷關溫泉會1日",imgUrl:"https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",area:"台中",description:"全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",group:20,price:1580,rate:7},{id:3,name:"綠島自由行套裝行程",imgUrl:"https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_1.png?raw=true",area:"東部",description:" 嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合。",group:87,price:6800,rate:10},{id:4,name:"清境高空觀景步道",imgUrl:"https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_4.png?raw=true",area:"台中",description:"清境農場青青草原數十公頃碧草，這些景觀豐沛了清境觀景步道的風格，也涵養它無可取代的特色。",group:26,price:1400,rate:2},{id:5,name:"山林悠遊套票",imgUrl:"https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_3.png?raw=true",area:"台中",description:"山林悠遊套票，結合南投清境高空步道、雙龍瀑布七彩吊橋、瑞龍瀑布園區之熱門景點。",group:20,price:1765,rate:7}],p=[{text:"全部地區",value:"全部"},{text:"台北",value:"台北"},{text:"台中",value:"台中"},{text:"高雄",value:"高雄"},{text:"東部",value:"東部"},{text:"台灣海峽",value:"台灣海峽"}];function f(e){const l=document.getElementById(e.inputFormId);document.getElementById(e.addNewCardId),l.querySelectorAll("input, select, textarea").forEach(r=>{const a=r.parentElement.parentElement.querySelector(`.${e.alertDivClass}`);r.addEventListener("input",function(){this.type!=="hidden"&&(this.validity.valid?a.classList.toggle("hidden",!0):a.classList.toggle("hidden",!1))})})}const g=e=>`
+<li class="ticketCard">
+          <div class="ticketCard-img">
+            <a href="#">
+              <img
+                src="${e.imgUrl}"
+                alt=""
+              />
+            </a>
+            <div class="ticketCard-region">${e.area}</div>
+            <div class="ticketCard-rank">${e.rate}</div>
+          </div>
+          <div class="ticketCard-content">
+            <div>
+              <h3>
+                <a href="#" class="ticketCard-name">${e.name}</a>
+              </h3>
+              <p class="ticketCard-description">
+                ${e.description}
+              </p>
+            </div>
+            <div class="ticketCard-info">
+              <p class="ticketCard-num">
+                <span><i class="fas fa-exclamation-circle"></i></span>
+                剩下最後 <span id="ticketCard-num"> ${e.group} </span> 組
+              </p>
+              <p class="ticketCard-price">
+                TWD <span id="ticketCard-price">$${e.price}</span>
+              </p>
+            </div>
+          </div>
+        </li>
+`,h=e=>`
+<option value="${e.value}">${e.text}</option>
+`,c='<option value="" disabled selected hidden>地區搜尋</option>',n={areaSelectId:"searchByArea",areaSelectInAddId:"ticketRegion",cardContainerId:"ticketCards",addCardSucessMsgId:"addNewCardSuccess",canotFindAreaId:"cantFind-area",filterResultId:"searchResult-text",validator_Constants:{inputFormId:"newTicketForm",alertDivClass:"alert-message",addNewCardId:"addNewCard"},newTicketFormId:"newTicketForm",selectAllValue:"全部",loadJson1BtnId:"loadLV1",loadJson2BtnId:"loadLV2",json1Url:"https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json",json2Url:"https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json",modalOverlayId:"success-modal-overlay"};let u;function v(){I(),b(),y(),d(n.selectAllValue),w([{btnId:n.loadJson1BtnId,jsonUrl:n.json1Url,level:"LV1"},{btnId:n.loadJson2BtnId,jsonUrl:n.json2Url,level:"LV2"}])}function y(){const e=document.getElementById(n.areaSelectInAddId),l=document.getElementById(n.areaSelectId);l.addEventListener("change",t=>d(t.target.value));let r=c,a=c;p.forEach((t,i)=>{const o=h(t);r+=o,i!=0&&(a+=o)}),e.innerHTML=a,l.innerHTML=r}function d(e){const l=document.getElementById(n.cardContainerId),r=document.getElementById(n.filterResultId);let a="",t=0;s.forEach((i,o)=>{(e===n.selectAllValue||i.area===e)&&(a+=g(i),t++)}),l.innerHTML=a,r.innerText=`本次搜尋共 ${t} 筆資料`,x(t)}function I(){document.getElementById(n.validator_Constants.addNewCardId).addEventListener("click",E),f(n.validator_Constants)}function E(){const e=document.getElementById(n.newTicketFormId);if(!e.reportValidity())return;const l=new FormData(e),r=e.querySelectorAll("[name][property]"),a={};a.id=s.length,r.forEach(t=>{const i=t.getAttribute("name"),o=t.getAttribute("property");a[o]=l.get(i)}),s.push(a),d(n.selectAllValue),document.getElementById(n.addCardSucessMsgId),m("套票新增成功！",void 0,()=>{e.reset(),e.querySelectorAll(`.${n.validator_Constants.alertDivClass}`).forEach(t=>{t.classList.toggle("hidden",!0)})}),document.getElementById(n.areaSelectId).value=n.selectAllValue}function w(e){e.forEach(l=>{document.getElementById(l.btnId).addEventListener("click",()=>C(l.level,l.jsonUrl))})}function C(e,l,r=5e3){axios.get(l,{timeout:r}).then(function(a){s.length=0,Array.prototype.push.apply(s,e==="LV1"?a.data:a.data.data),d(n.selectAllValue);const t=`下載 ${e} 資料，成功載入`;m(t),document.getElementById(n.areaSelectId).value=n.selectAllValue}).catch(function(a){k();let t=a.message;a.stack&&(t+=a.stack),a.response&&(t+=a.response),a.request&&(errMst+=a.request),alert(`載入 JSON ${e} 失敗；請檢察網路連線、或URL ${l}是否正確`)})}function x(e){const l=document.getElementById(n.canotFindAreaId);l.style.display=e==0?"block":"none"}function b(){const e=document.querySelector(`#${n.modalOverlayId} .modal`);e.addEventListener("click",()=>{clearTimeout(u),e.parentElement.classList.remove("active")})}function m(e,l=3e3,r=null){const a=document.getElementById(n.modalOverlayId);a.querySelector(".alt-msg-text").textContent=e,a.classList.add("active"),u=setTimeout(function(){a.classList.remove("active"),r!=null&&r()},l)}function k(e=null){document.getElementById(n.modalOverlayId).classList.remove("active")}console.log("JS week6 homework");document.addEventListener("DOMContentLoaded",v);
